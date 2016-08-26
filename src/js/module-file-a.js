8d4891028a6dad3f6a1c2369b02c76675c9b85b3
Apollo Tang <tang.apollo@gmail.com>
2016-08-25 22:18:55 -0400

    adjust position of the map to show less of antartica

--- src/js/module-file-a.js
+++ src/js/module-file-a.js
@@ -1,7 +1,7 @@
 import d3 from 'd3';
 
 const w_svg = 3000;
-const h_svg = 2000;
+const h_svg = 1800;
 
 var viewBoxMaxX = w_svg;
 var viewBoxMaxY = h_svg;
@@ -18,8 +18,10 @@ selection.svg = svgContainer;
 
 d3.json("/data/world.geojson", createMap);
 
+        const translation_h = w_svg/2;
+        const translation_v = h_svg/2 * 1.1;
         function createMap(countries) {
-            var aProjection = d3.geo.mercator().scale(480).translate([w_svg/2,h_svg/2]);
+            var aProjection = d3.geo.mercator().scale(480).translate([translation_h,translation_v]);
             var geoPath = d3.geo.path().projection(aProjection);
             d3.select("svg").selectAll("path").data(countries.features)
                 .enter()
